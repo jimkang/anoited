59e4c9f4eac785b2bc66a0b579674be19af17e79
Jim Kang <jimkang@gmail.com>
2022-02-12 21:55:06 -0500

    Further Noitification.

--- anoited-config.js
+++ anoited-config.js
@@ -9,10 +9,12 @@ module.exports = {
     maxEntriesPerPage: 25,
     fileAbstractionType: "fs",
     footerHTML: `<div class="important-info">
-    <a href="https://smidgeo.com/anoited/rss/index.rss">RSS feed</a> |
-    <a href="https://smidgeo.com/notes/deathmtn/">Main microblog</a>
+    <a href="https://smidgeo.com/anoited/">Anoited index page</a> |
+    <a href="https://smidgeo.com/anoited/rss/index.rss">Anoited RSS feed</a> |
+    <a href="https://smidgeo.com/notes/deathmtn/">deathmtn</a>
     </div>
     `,
+    headerExtraHTML: `<div>These are posts about my tourist-style expeditions into <a href="https://noitagame.com/">Noita</a>. I'm not good at the game, but I really enjoy seeing weird things and dying in wacky and varied ways.`,
     generateRSS: true,
     archiveBaseURL: "https://smidgeo.com/anoited",
     mediaDir: "https://smidgeo.com/notes/deathmtn/media",
